e4327b0e232d860050f30d4616c6631720416843
tsukasa konno <ugfg0283@gmail.com>
2021-10-04 12:38:43 +0000

    add fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,16 @@
 def fizzbuzz(number)
     if number % 3 == 0 && number % 5 == 0
-        puts "FizzBuzz"
+        return "FizzBuzz"
     elsif number % 5 == 0
-        puts "Buzz"
+        return "Buzz"
     elsif number % 3 == 0
-        puts "Fizz"
+        return "Fizz"
     else
-        puts number
+        return number
     end
 end
+
 num_max = 100
 (1..num_max).each do |number|
-    fizzbuzz(number)
+    puts fizzbuzz(number)
 end
